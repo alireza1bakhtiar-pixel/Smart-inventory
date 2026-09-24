ded2db4881ac9911f4afd536cafd25475e931c72
Alireza <alireza1bakhtiar@gmail.comm>
2026-09-24 14:47:10 +0330

    save , load & remove def added

--- src/main.py
+++ src/main.py
@@ -1,21 +1,88 @@
+import csv
 from product import Product
+
+product_list = []
+
+
+def save_product(product):
+    with open("products.csv", "a", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(
+            [
+                product.id,
+                product.name,
+                product.category,
+                product.price,
+                product.quantity,
+            ]
+        )
+        product_list.append(p)
+
+
+def load_product():
+    try:
+        with open("products.csv", "r", newline="") as file:
+            reader = csv.reader(file)
+            for row in reader:
+                p = Product(int(row[0]), row[1], row[2], int(row[3]), int(row[4]))
+                product_list.append(p)
+    except FileNotFoundError:
+        print("file not found !!")
+
+
+def remove_product():
+    print("1. Remove by ID")
+    print("2. Remove by Name")
+    print("3. Remove by Category")
+
+    choise = int(input("Enter your choice: "))
+    if choise == 1:
+        product_id = int(input("enter product id: "))
+        found = False
+        for i in product_list:
+            if product_id == i.id:
+                product_list.remove(i)
+                found = True
+                break
+        if found:
+            print("product remowed successfully.")
+            rewrite_products()    
+        else:
+            print("ID not found")    
+
+def rewrite_products():
+    with open("products.csv","w",newline="")as file :
+        writer = csv.writer(file)
+        for i in product_list:
+            writer.writerow([i.id,i.name,i.category,i.price,i.quantity])
+
+
+
+load_product()
+
 print("*$* WELCOME TO SMART INVENTORY *$*")
+
+
 while True:
-    print("1.ADD PRODUCT\n" \
-    "2.REMOVE PRODUCT\n" \
-    "3.UPDATE PRODUCT\n" \
-    "4.SEARCH PRODUCT\n" \
-    "5.SHOW ALL PRODUCTS\n" \
-    "6.SHOW PRODUCTS VALUE\n" \
-    "7.SAVE/LOAD DATA")
+
+    print(
+        "1.ADD PRODUCT\n"
+        "2.REMOVE PRODUCT\n"
+        "3.UPDATE PRODUCT\n"
+        "4.SEARCH PRODUCT\n"
+        "5.SHOW ALL PRODUCTS\n"
+        "6.SHOW PRODUCTS VALUE\n"
+        "7.SAVE/LOAD DATA"
+    )
+
     choise = int(input("please enter your choise :"))
     if choise == 1:
         p_id = int(input("Enter ID : "))
-        p_name = input("Enter Product Name: ") 
+        p_name = input("Enter Product Name: ")
         p_category = input("Enter Product Category: ")
         p_price = int(input("Enter Product price: "))
-        p_quantity = int(input("Enter Quantity of Product"))
+        p_quantity = int(input("Enter Quantity of Product: "))
 
-        p = Product(p_id,p_name,p_category,p_price,p_quantity)
-        print(p)
-            
+        p = Product(p_id, p_name, p_category, p_price, p_quantity)
+
+        save_product(p)
